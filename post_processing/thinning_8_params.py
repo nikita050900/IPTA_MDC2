b03980eb7c2f036ebfc894ec7a4b92c7d162b3cd
Nikita Agarwal <nikita.agarwal@nanograv.org>
2025-08-29 17:11:43 -0400

    My new changes

--- post_processing/thinning_8_params.py
+++ post_processing/thinning_8_params.py
@@ -1,141 +1,53 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-#get_ipython().run_line_magic('matplotlib', 'inline')
-#get_ipython().run_line_magic('config', "InlineBackend.figure_format = 'retina'")
-#get_ipython().run_line_magic('load_ext', 'autoreload')
-#%load_ext line_profiler
-#%load_ext snakeviz
-#get_ipython().run_line_magic('autoreload', '2')
-
+#!/usr/bin/env python3
+import argparse
 import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
-import corner
-
-import pickle
-
-import enterprise
-from enterprise.pulsar import Pulsar
-import enterprise.signals.parameter as parameter
-from enterprise.signals import utils
-from enterprise.signals import signal_base
-from enterprise.signals import selections
-from enterprise.signals.selections import Selection
-from enterprise.signals import white_signals
-from enterprise.signals import gp_signals
-from enterprise.signals import deterministic_signals
-import enterprise.constants as const
-
-from enterprise_extensions import deterministic
-
-from scipy.stats import norm
-
-import libstempo as T2
-import libstempo.toasim as LT
-import libstempo.plot as LP
-
-import glob
-import json
 import h5py
-import healpy as hp
-import scipy.constants as sc
-import emcee
-
-from numba.typed import List
-
 import sys
-import h5py
+from pathlib import Path
 
+# Hardcoded base directory and number of parameters
+BASE_DIR = Path("/scratch/na00078/projects/IPTA_MDC2/h5_files/")
+FIRST_N = 8
 
-# In[2]:
+def main():
+    ap = argparse.ArgumentParser(description="Thin an HDF5 chain file.")
+    ap.add_argument("--infile", required=True, help="Input HDF5 filename (just the name, not path)")
+    ap.add_argument("--outfile", required=True, help="Output HDF5 filename (just the name, not path)")
+    args = ap.parse_args()
 
-'''
-#load psr pickles
+    infile_path = BASE_DIR / args.infile
+    outfile_path = BASE_DIR / args.outfile
 
-#make sure this points to the same pickled pulsars we used for the MCMC
-data_pkl = '/scratch/js0311/15yr_data/15yrCW/v1p1_de440_pint_bipm2019_unshifted_pdist.pkl'
+    if not infile_path.exists():
+        print(f"ERROR: Infile does not exist: {infile_path}", file=sys.stderr)
+        sys.exit(1)
 
-#with open('nanograv_11yr_psrs_old.pkl', 'rb') as psr_pkl:
-with open(data_pkl, 'rb') as psr_pkl:
-    psrs = pickle.load(psr_pkl)
-    
-print(len(psrs))
+    print(f"Infile:  {infile_path}")
+    print(f"Outfile: {outfile_path}")
+    print(f"Keeping first {FIRST_N} parameters")
 
+    with h5py.File(infile_path, "r") as f:
+        Ts = f["T-ladder"][...]
+        samples_cold = f["samples_cold"][:, :, :]
+        log_likelihood = f["log_likelihood"][:1, :]
+        par_names = [x.decode("UTF-8") for x in list(f["par_names"])]
+        acc_fraction = f["acc_fraction"][...]
+        fisher_diag = f["fisher_diag"][...]
 
-# In[3]:
+        print("Infile samples_cold shape:", samples_cold.shape)
+        print("Infile log_likelihood shape:", log_likelihood.shape)
 
+        k = min(FIRST_N, samples_cold.shape[-1])
 
-#load psr names only if we want to save RAM
-class psr_name:
-    def __init__(self, name):
-        self.name = name
+    with h5py.File(outfile_path, "w") as f:
+        f.create_dataset("samples_cold", data=samples_cold[:, :, :k], compression="gzip", chunks=True)
+        f.create_dataset("log_likelihood", data=log_likelihood[:, :], compression="gzip", chunks=True)
+        f.create_dataset("par_names", data=np.array(par_names, dtype="S"))
+        f.create_dataset("acc_fraction", data=acc_fraction)
+        f.create_dataset("fisher_diag", data=fisher_diag)
+        f.create_dataset("T-ladder", data=Ts)
 
-psrListFile = "/scratch/js0311/15yr_data/15yr_v1_1/psrlist_15yr_pint.txt"
+    print("Done.")
 
-psrs = []
-with open(psrListFile, 'r') as fff:
-    for line in fff:
-        psrname = line.strip()
-        #print(psrname)
-        psrs.append(psr_name(psrname))
-        
-print(len(psrs))
-for i,psr in enumerate(psrs):
-    print(str(i) + ": " + psr.name)
-
-
-# In[4]:
-
-
-pwd
-
-
-# ## Generate Outfile
-
-# In[5]:
-
-
-#'''
-#COMMENT IF OUTFILE GENERATED
-#Thinning script for one source
-i = "/scratch/js0311/jet_source_project_results/binary_model_detect_runs/3C120.h5"
-
-infile = i
-first_n_param = 8
-outfile = '/scratch/js0311/jet_source_project_results/binary_model_detect_runs/3C120_outfile.h5'
-
-print(infile)
-print(first_n_param)
-print(outfile)
-
-with h5py.File(infile, 'r') as f:
-    Ts = f['T-ladder'][...]
-    samples_cold = f['samples_cold'][:,:,:]
-    print("Infile sample shape", samples_cold[-1].shape)
-    log_likelihood = f['log_likelihood'][:1,:]
-    print("Infile loglikelihood shape", log_likelihood.shape)
-    par_names = [x.decode('UTF-8') for x in list(f['par_names'])]
-    acc_fraction = f['acc_fraction'][...]
-    fisher_diag = f['fisher_diag'][...]
-
-with h5py.File(outfile, 'w') as f:
-    f.create_dataset('samples_cold', data=samples_cold[:,:,:first_n_param], compression="gzip", chunks=True)
-    f.create_dataset('log_likelihood', data=log_likelihood[:,:], compression="gzip", chunks=True)
-    f.create_dataset('par_names', data=np.array(par_names, dtype='S'))
-    f.create_dataset('acc_fraction', data=acc_fraction)
-    f.create_dataset('fisher_diag', data=fisher_diag)
-    f.create_dataset('T-ladder', data=Ts)
-#'''
-###################################################
-
-'''
-
-
-
-
-
-
+if __name__ == "__main__":
+    main()
