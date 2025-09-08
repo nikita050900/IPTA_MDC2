de312495b37cb73bfb28af659cae1895df74467a
Nikita Agarwal <nikita.agarwal@nanograv.org>
2025-09-08 14:52:37 -0400

    My new changes

--- post_processing/thinning_8_multiple_files.sh
+++ post_processing/thinning_8_multiple_files.sh
@@ -9,16 +9,23 @@ PY="/scratch/na00078/conda_envs/QuickCW/bin/python"
 
 # List your input filenames (no paths)
 FILES=(
-  "3C120.h5"
-  "NGC3115.h5"
-  "3C66B.h5"
+  "G2D2_narrow_detect.h5"
+  "G2D2_broad_detect.h5"
+  "G2D1_narrow_detect.h5"
+  "G2D1_broad_detect.h5"
+  "G2D2_detect_allsky.h5"
+  "G2D2_broad_UL.h5"
+  "G2D2_narrow_UL.h5"
+  "G2D1_broad_UL.h5"
+  "G2D1_narrow_UL.h5"
+  "G2D2_UL_allsky.h5"
 )
 
 for IN in "${FILES[@]}"; do
   # make outfile name by suffixing _thinned
   OUT="${IN%.h5}_outfile.h5"
   echo "Processing $IN -> $OUT"
-  $PY thinning_8_paras.py --infile "$IN" --outfile "$OUT"
+  $PY thinning_8_params.py --infile "$IN" --outfile "$OUT"
 done
 
 echo "All done."
